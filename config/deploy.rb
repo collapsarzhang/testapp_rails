4ed8621352a1495cbc7e3faa7382ef7103d54c54
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2015-06-14 18:12:31 -0700

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # Change these
-server '178.62.88.94', port: 7171, roles: [:web, :app, :db], primary: true
+server '104.160.40.199', port: 27002, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:sheharyarn/testapp_rails.git'
+set :repo_url,        'git@github.com:collapsarzhang/testapp_rails.git'
 set :application,     'testapp'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
@@ -82,4 +82,3 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
